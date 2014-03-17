3230946616e91e1f1cf6fb498c5e4f4be979572f
Nina Breznik <ninabreznik@gmail.com>
2014-03-17 16:40:58 +0100

    Fully working app

--- api/policies/authenticated.js
+++ api/policies/authenticated.js
@@ -1,27 +1,21 @@
 /**
- * isAuthenticated
- *
- * @module      :: Policy
- * @description :: Simple policy to allow any authenticated user
- *                 Assumes that your login action in one of your controllers sets `req.session.authenticated = true;`
- * @docs        :: http://sailsjs.org/#!documentation/policies
- *
+ * Allow any authenticated user.
  */
-module.exports = function(req, res, next) {
+module.exports = function(req, res, ok) {
 
-  // User is allowed, proceed to the next policy, 
-  // or if this is the last policy, the controller
-  if (req.session.authenticated) {
-    return next();
+  // User is allowed, proceed to controller
+  if (req.session.User) {
+    return ok();
   }
 
   // User is not allowed
-  // (default res.forbidden() behavior can be overridden in `config/403.js`)
   else {
-    var requireLoginError = [{name: 'requireLogin', message: 'You must be signed in"'}]
-    req.session.flash = {
-      err: requireLoginError
-    }
-    res.redirect('/session/new');
+    //  var requireLoginError = [{name: 'requireLogin', message: 'You must be signed in.'}]
+    // req.session.flash = {
+    //  err: requireLoginError
+    // }
+    // res.redirect('/session/new');
+    //   return;
+    res.send(403);
   }
 };
