01d4a15684f5591c6bddf2726f5cdfe0220fa4e4
Nina Breznik <ninabreznik@gmail.com>
2014-03-19 13:37:23 +0100

    Modify MessageController, layout.ejs and index.ejs

--- api/controllers/MessageController.js
+++ api/controllers/MessageController.js
@@ -17,14 +17,10 @@
 
 module.exports = {
     
-  
-
-
-  /**
-   * Overrides for the settings in `config/controllers.js`
-   * (specific to MessageController)
-   */
-  _config: {}
-
-  
+index: function (req, res) {
+    res.view('message/index', {
+      user: true,
+      error: false
+    });
+  }
 };
